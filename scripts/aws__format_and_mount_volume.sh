aab0da6241019305daf0e52c8445bf89290a647e
drosofff <drosofff@gmail.com>
2017-04-30 11:39:21 +0200

    add aws__format_and_mount_volume.sh and improve gce_format_and_mount_volume.sh

--- scripts/aws__format_and_mount_volume.sh
+++ scripts/aws__format_and_mount_volume.sh
@@ -3,15 +3,16 @@
 
 EXTRA_VOLUME=galaxy-data # adapt to the name of the GCE extra volume available on the web interface
 # Be careful to point to the correct interface (here sdb with one extra volume)
-DEVICE=/dev/sdb
+DEVICE=/dev/xvdb
 
-# see https://cloud.google.com/compute/docs/disks/add-persistent-disk for adding persistent disk to gce instances
+# see http://docs.aws.amazon.com/en_en/AWSEC2/latest/UserGuide/ebs-using-volumes.html for adding persistent disk to aws instances
 
 # Format and attach an extra volume (ext4 formated) to the gce main instance
-mkfs.ext4 -F -E lazy_itable_init=0,lazy_journal_init=0,discard $DEVICE/by-id/google-$EXTRA_VOLUME
+mkfs.ext4 -F -E lazy_itable_init=0,lazy_journal_init=0,discard $DEVICE
 mkdir -p /mnt/$EXTRA_VOLUME
-mount -o discard,defaults /dev/disk/by-id/google-$EXTRA_VOLUME /mnt/$EXTRA_VOLUME
+mount -o discard,defaults $DEVICE /mnt/$EXTRA_VOLUME
 chmod a+w /mnt/$EXTRA_VOLUME
 
 # to get persistent mounting
 echo UUID=`sudo blkid -s UUID -o value $DEVICE` /mnt/$EXTRA_VOLUME ext4 discard,defaults,nobootwait 0 2 | sudo tee -a /etc/fstab
+
